af45f12304c4baac91e2332d9093fdfeb1ba2726
Jean-Charles <jeancharles@geekarts.fr>
2016-09-09 16:44:46 +0000

    ajout de l'option mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,14 @@ class  ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
+             name = params[:contact][:name]
+             email = params[:contact][:email]
+             body = params[:contact][:comments]
+             
+             ContactMailer.contact_email(name, email, body).deliver
+                    
+
+            
             flash[:success] = "Bravo!"
             redirect_to new_contacts_path
         else
