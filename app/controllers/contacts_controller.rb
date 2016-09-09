d39efa20ee82a80830563ad1b213ac9eb31b2918
Jean-Charles <jeancharles@geekarts.fr>
2016-09-09 15:18:29 +0000

    ajout de validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,21 @@ class  ContactsController < ApplicationController
     
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Bravo!"
+            redirect_to new_contacts_path
+        else
+               flash[:danger] = "Y a un bug !"
+            redirect_to new_contacts_path
+        end
+            
     end
     
+    private
+        def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+        end
+        
 end
